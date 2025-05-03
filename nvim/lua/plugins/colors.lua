043245c6cfed4291afe9cbac111a4a92b0d7e3ae
Siateus <tecmathe@gmail.com>
2025-05-03 18:55:19 -0300

    update

---
+++ nvim/lua/plugins/colors.lua
@@ -0,0 +1,21 @@
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	lazy = false,
+-- 	name = "gruvbox",
+-- 	priority = 997,
+-- 	config = function()
+-- 		vim.cmd.colorscheme("gruvbox")
+-- 	end,
+-- }
+
+
+
+return {
+    "AlphaTechnolog/pywal.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        
+    end,
+}
+
